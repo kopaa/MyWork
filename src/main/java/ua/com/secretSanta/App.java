44dc8c899d99924b048c4a594faa29748a591871
okovitiy <okovitiy@gmail.com>
2014-02-03 07:58:05 +0200

    +

---
+++ src/main/java/ua/com/secretSanta/App.java
@@ -0,0 +1,27 @@
+package main.java.ua.com.secretSanta;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * User: Andrew
+ * Date: 11.01.14
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        try {
+            SecretSantaJdbc s = new SecretSantaJdbc(ConnectionDB.getConnection("root", "andrew85"));
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            while (s.isExit()) {
+                System.out.println(s.getMenu());
+                s.createCommand(input.readLine());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionDB.closeConnection();
+            System.out.println("");
+        }
+    }
+}
